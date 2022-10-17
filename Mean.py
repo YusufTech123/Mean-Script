3e666d3a4540864a4ab09ad8f1491bc9a3bbce1a
yusuf <yusufghazali.yg@gmail.com>
2022-10-17 22:34:14 +0700

    Script-Mean

---
+++ Mean.py
@@ -0,0 +1,154 @@
+print("=" * 25)
+print("Kalkulator pencari mean")
+print("Programmer : Yusuf Ghazali")
+print("=" * 25)
+print("masukkan angka 1 jika ingin memasukkan 2 nilai angka")
+print("masukkan angka 1 jika ingin memasukkan 3 nilai angka")
+print("masukkan angka 1 jika ingin memasukkan 4 nilai angka")
+print("masukkan angka 1 jika ingin memasukkan 5 nilai angka")
+print("masukkan angka 1 jika ingin memasukkan 6 nilai angka")
+print("masukkan angka 1 jika ingin memasukkan 7 nilai angka")
+print("masukkan angka 1 jika ingin memasukkan 8 nilai angka")
+print("masukkan angka 1 jika ingin memasukkan 9 nilai angka")
+print("masukkan angka 1 jika ingin memasukkan 10 nilai angka")
+print("masukkan angka 1 jika ingin memasukkan 11 nilai angka")
+print("masukkan angka 1 jika ingin memasukkan 12 nilai angka")
+
+pilihanUser = eval(input(
+    "pilih jumlah nilai angka yang anda inginkan dengan memasukkan sesuai nomornya:  "))
+
+convertIntegerPilihanUser = int(pilihanUser)
+
+if (convertIntegerPilihanUser == 2):
+    nilaiInput1 = eval(input("Masukkan angka pertama! :  "))
+    nilaiInput2 = eval(input("Masukkan angka kedua! :  "))
+    jumlahNilaiPilihanUser = int(nilaiInput1) + int(nilaiInput2)
+
+elif (convertIntegerPilihanUser == 3):
+    nilaiInput1 = eval(input("Masukkan angka pertama! :  "))
+    nilaiInput2 = eval(input("Masukkan angka kedua! :  "))
+    nilaiInput3 = eval(input("Masukkan angka ketiga! :  "))
+    jumlahNilaiPilihanUser = int(nilaiInput1) + \
+        int(nilaiInput2) + int(nilaiInput3)
+
+elif (convertIntegerPilihanUser == 4):
+    nilaiInput1 = eval(input("Masukkan angka pertama! :  "))
+    nilaiInput2 = eval(input("Masukkan angka kedua! :  "))
+    nilaiInput3 = eval(input("Masukkan angka ketiga! :  "))
+    nilaiInput4 = eval(input("Masukkan angka keempat! :  "))
+    jumlahNilaiPilihanUser = int(
+        nilaiInput1) + int(nilaiInput2) + int(nilaiInput3) + int(nilaiInput4)
+
+elif (convertIntegerPilihanUser == 5):
+    nilaiInput1 = eval(input("Masukkan angka pertama! :  "))
+    nilaiInput2 = eval(input("Masukkan angka kedua! :  "))
+    nilaiInput3 = eval(input("Masukkan angka ketiga! :  "))
+    nilaiInput4 = eval(input("Masukkan angka keempat! :  "))
+    nilaiInput5 = eval(input("Masukkan angka kelima! :  "))
+    jumlahNilaiPilihanUser = int(nilaiInput1) + int(nilaiInput2) + \
+        int(nilaiInput3) + int(nilaiInput4) + int(nilaiInput5)
+
+elif (convertIntegerPilihanUser == 6):
+    nilaiInput1 = eval(input("Masukkan angka pertama! :  "))
+    nilaiInput2 = eval(input("Masukkan angka kedua! :  "))
+    nilaiInput3 = eval(input("Masukkan angka ketiga! :  "))
+    nilaiInput4 = eval(input("Masukkan angka keempat! :  "))
+    nilaiInput5 = eval(input("Masukkan angka kelima! :  "))
+    nilaiInput6 = eval(input("Masukkan angka keenam! :  "))
+    jumlahNilaiPilihanUser = int(nilaiInput1) + int(nilaiInput2) + int(
+        nilaiInput3) + int(nilaiInput4) + int(nilaiInput5) + int(nilaiInput6)
+
+elif (convertIntegerPilihanUser == 7):
+    nilaiInput1 = eval(input("Masukkan angka pertama! :  "))
+    nilaiInput2 = eval(input("Masukkan angka kedua! :  "))
+    nilaiInput3 = eval(input("Masukkan angka ketiga! :  "))
+    nilaiInput4 = eval(input("Masukkan angka keempat! :  "))
+    nilaiInput5 = eval(input("Masukkan angka kelima! :  "))
+    nilaiInput6 = eval(input("Masukkan angka keenam! :  "))
+    nilaiInput7 = eval(input("Masukkan angka ketujuh! :  "))
+    jumlahNilaiPilihanUser = int(nilaiInput1) + int(nilaiInput2) + int(nilaiInput3) + int(
+        nilaiInput4) + int(nilaiInput5) + int(nilaiInput6) + int(nilaiInput7)
+
+elif (convertIntegerPilihanUser == 8):
+    nilaiInput1 = eval(input("Masukkan angka pertama! :  "))
+    nilaiInput2 = eval(input("Masukkan angka kedua! :  "))
+    nilaiInput3 = eval(input("Masukkan angka ketiga! :  "))
+    nilaiInput4 = eval(input("Masukkan angka keempat! :  "))
+    nilaiInput5 = eval(input("Masukkan angka kelima! :  "))
+    nilaiInput6 = eval(input("Masukkan angka keenam! :  "))
+    nilaiInput7 = eval(input("Masukkan angka ketujuh! :  "))
+    nilaiInput8 = eval(input("Masukkan angka kedelapan! :  "))
+    jumlahNilaiPilihanUser = int(nilaiInput1) + int(nilaiInput2) + int(nilaiInput3) + int(
+        nilaiInput4) + int(nilaiInput5) + int(nilaiInput6) + int(nilaiInput7) + int(nilaiInput8)
+
+elif (convertIntegerPilihanUser == 9):
+    nilaiInput1 = eval(input("Masukkan angka pertama! :  "))
+    nilaiInput2 = eval(input("Masukkan angka kedua! :  "))
+    nilaiInput3 = eval(input("Masukkan angka ketiga! :  "))
+    nilaiInput4 = eval(input("Masukkan angka keempat! :  "))
+    nilaiInput5 = eval(input("Masukkan angka kelima! :  "))
+    nilaiInput6 = eval(input("Masukkan angka keenam! :  "))
+    nilaiInput7 = eval(input("Masukkan angka ketujuh! :  "))
+    nilaiInput8 = eval(input("Masukkan angka kedelapan! :  "))
+    nilaiInput9 = eval(input("Masukkan angka kesembilan! :  "))
+    jumlahNilaiPilihanUser = int(nilaiInput1) + int(nilaiInput2) + int(nilaiInput3) + int(nilaiInput4) + int(
+        nilaiInput5) + int(nilaiInput6) + int(nilaiInput7) + int(nilaiInput8) + int(nilaiInput9)
+
+elif (convertIntegerPilihanUser == 10):
+    nilaiInput1 = eval(input("Masukkan angka pertama! :  "))
+    nilaiInput2 = eval(input("Masukkan angka kedua! :  "))
+    nilaiInput3 = eval(input("Masukkan angka ketiga! :  "))
+    nilaiInput4 = eval(input("Masukkan angka keempat! :  "))
+    nilaiInput5 = eval(input("Masukkan angka kelima! :  "))
+    nilaiInput6 = eval(input("Masukkan angka keenam! :  "))
+    nilaiInput7 = eval(input("Masukkan angka ketujuh! :  "))
+    nilaiInput8 = eval(input("Masukkan angka kedelapan! :  "))
+    nilaiInput9 = eval(input("Masukkan angka kesembilan! :  "))
+    nilaiInput10 = eval(input("Masukkan angka kesepuluh! :  "))
+    jumlahNilaiPilihanUser = int(nilaiInput1) + int(nilaiInput2) + int(nilaiInput3) + int(nilaiInput4) + int(
+        nilaiInput5) + int(nilaiInput6) + int(nilaiInput7) + int(nilaiInput8) + int(nilaiInput9) + int(nilaiInput10)
+
+elif (convertIntegerPilihanUser == 11):
+    nilaiInput1 = eval(input("Masukkan angka pertama! :  "))
+    nilaiInput2 = eval(input("Masukkan angka kedua! :  "))
+    nilaiInput3 = eval(input("Masukkan angka ketiga! :  "))
+    nilaiInput4 = eval(input("Masukkan angka keempat! :  "))
+    nilaiInput5 = eval(input("Masukkan angka kelima! :  "))
+    nilaiInput6 = eval(input("Masukkan angka keenam! :  "))
+    nilaiInput7 = eval(input("Masukkan angka ketujuh! :  "))
+    nilaiInput8 = eval(input("Masukkan angka kedelapan! :  "))
+    nilaiInput9 = eval(input("Masukkan angka kesembilan! :  "))
+    nilaiInput10 = eval(input("Masukkan angka kesepuluh! :  "))
+    nilaiInput11 = eval(input("Masukkan angka kesebelas! :  "))
+    jumlahNilaiPilihanUser = int(nilaiInput1) + int(nilaiInput2) + int(nilaiInput3) + int(nilaiInput4) + int(nilaiInput5) + int(
+        nilaiInput6) + int(nilaiInput7) + int(nilaiInput8) + int(nilaiInput9) + int(nilaiInput10) + int(nilaiInput11)
+
+elif (convertIntegerPilihanUser == 12):
+    nilaiInput1 = eval(input("Masukkan angka pertama! :  "))
+    nilaiInput2 = eval(input("Masukkan angka kedua! :  "))
+    nilaiInput3 = eval(input("Masukkan angka ketiga! :  "))
+    nilaiInput4 = eval(input("Masukkan angka keempat! :  "))
+    nilaiInput5 = eval(input("Masukkan angka kelima! :  "))
+    nilaiInput6 = eval(input("Masukkan angka keenam! :  "))
+    nilaiInput7 = eval(input("Masukkan angka ketujuh! :  "))
+    nilaiInput8 = eval(input("Masukkan angka kedelapan! :  "))
+    nilaiInput9 = eval(input("Masukkan angka kesembilan! :  "))
+    nilaiInput10 = eval(input("Masukkan angka kesepuluh! :  "))
+    nilaiInput11 = eval(input("Masukkan angka kesebelas! :  "))
+    nilaiInput12 = eval(input("Masukkan angka keduabelas! :  "))
+    jumlahNilaiPilihanUser = int(nilaiInput1) + int(nilaiInput2) + int(nilaiInput3) + int(nilaiInput4) + int(nilaiInput5) + int(
+        nilaiInput6) + int(nilaiInput7) + int(nilaiInput8) + int(nilaiInput9) + int(nilaiInput10) + int(nilaiInput11) + int(nilaiInput12)
+
+else:
+    print("Angka yang anda masukkan tidak valid!")
+
+
+inputAngkaFrekuensi = pilihanUser
+
+operasiKalkulasiMean = jumlahNilaiPilihanUser / inputAngkaFrekuensi
+
+print("=" * 25)
+print(f"Hasil Meannya adalah : {operasiKalkulasiMean}")
+print("=" * 25)
+
+exitProgram = input("Tekan enter sekali untuk keluar program....")
